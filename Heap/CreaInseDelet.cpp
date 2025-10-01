31ff5a827f112ef18f995e54ff76ed742e9230ff
HarsH <jharsh0002@gmail.com>
2025-10-01 19:24:38 +0530

    heapsort

--- Heap/CreaInseDelet.cpp
+++ Heap/CreaInseDelet.cpp
@@ -69,10 +69,10 @@ void heapify(int arr[],int n, int i){
     int left = 2*i;
     int right = 2*i+1;
 
-    if(left < n && arr[largest ] < arr[left]){
+    if(left <= n && arr[largest ] < arr[left]){
         largest = left;
     }
-    if(right < n && arr[largest ] < arr[right]){
+    if(right <= n && arr[largest ] < arr[right]){
         largest = right;
     }
 
@@ -84,6 +84,15 @@ void heapify(int arr[],int n, int i){
 
 
 }
+void heapSort(int arr[],int n){
+    int size = n;
+    while(size > 1){
+
+        swap(arr[size],arr[1]);
+        size--;
+        heapify(arr,size,1);
+    }
+};
 int main(){
           heap h;
           h.insert(50);  
@@ -103,6 +112,13 @@ int main(){
         }
         cout<<"Printing the elements of heap"<< endl;
         for(int i=1;i<=n;i++){
+            cout<<arr[i]<<" ";
+        }cout<<endl;
+
+        heapSort(arr,n);
+
+        cout<<"Heap sorted array: "<<endl;
+          for(int i=1;i<=n;i++){
             cout<<arr[i]<<" ";
         }cout<<endl;
 
